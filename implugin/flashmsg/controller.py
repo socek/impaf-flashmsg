a4ef39a9ae67e015e512aeee497885c9c2f882b0
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2015-07-14 22:08:45 +0200

    More extensible controller.

--- implugin/flashmsg/controller.py
+++ implugin/flashmsg/controller.py
@@ -5,7 +5,8 @@ from .widget import FlashMessageWidget
 
 
 class FlashMessageController(Controller, FlashMessageRequestable):
+    _cls_flash_message_widget = FlashMessageWidget
 
     def _create_widgets(self):
         super()._create_widgets()
-        self.add_widget('flashmsg', FlashMessageWidget())
+        self.add_widget('flashmsg', self._cls_flash_message_widget())
